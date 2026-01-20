fe1709fd2a21506142d461e4c1f4b3c56638ad2e
Leonid Kostrykin <void@evoid.de>
2026-01-20 22:19:57 +0100

    Add tests for utils, fix bugs

--- gialint/utils.py
+++ gialint/utils.py
@@ -2,20 +2,26 @@ _types = {
     'text': str,
     'integer': int,
     'float': float,
-    'boolean': bool,
     'color': str,
     'hidden': str,
+    'select': str,
 }
 
 
 def get_full_param_name(param_xml):
     tokens = []
     current = param_xml
-    while current.tag != 'inputs':
+    while current.tag not in ('inputs', 'test'):
         if current_name := current.get('name'):
             tokens.append(current_name)
         current = current.getparent()
     return '.'.join(tokens[::-1])
+
+
+def _create_boolean_converter(param):
+    def _type(checked):
+        return param.attrib.get(f'{checked}value', checked)
+    return _type
 
 
 def get_test_inputs(inputs_xml, test_xml):
@@ -25,21 +31,27 @@ def get_test_inputs(inputs_xml, test_xml):
     # Parse input parameters, default values, and type converters
     for param in inputs_xml.findall('.//param'):
         full_param_name = get_full_param_name(param)
+        converter = None
 
         # Skip invalid parameters (this is handled by `planemo lint`)
         if (param_type := param.attrib.get('type', '').lower()) == '':
             continue
 
         # Read the value from the `value` attribute
-        if param_type in ('text', 'integer', 'float', 'boolean', 'color', 'hidden'):
+        if param_type in ('text', 'integer', 'float', 'color', 'hidden'):
             inputs[full_param_name] = param.attrib.get('value')
+
+        # Read the value from the `checked` attribute
+        if param_type in ('boolean',):
+            inputs[full_param_name] = param.attrib.get('checked', 'false')
+            converter = _create_boolean_converter(param)
 
         # Read the value from children elements
         if (default_options := param.findall('./option[@selected="true"]')):
             inputs[full_param_name] = default_options[0].attrib.get('value')
 
         # Register type converter
-        input_types[full_param_name] = _types.get(param_type)
+        input_types[full_param_name] = converter or _types.get(param_type)
 
     # Parse test parameters
     for param in test_xml.findall('.//param'):
@@ -54,7 +66,9 @@ def get_test_inputs(inputs_xml, test_xml):
 
     # Return inputs and apply type converters
     return {
-        key: input_types[key](value) for key, value in inputs.items()
+        key: (
+            None if value is None else input_types[key](value)
+        ) for key, value in inputs.items()
     }
 
 
---
+++ tests/test_utils.py
@@ -0,0 +1,95 @@
+import pathlib
+import unittest
+from lxml import etree
+
+from gialint import utils
+
+
+tools_root_path = pathlib.Path(__file__).parent / 'tools'
+
+
+class ToolTest(unittest.TestCase):
+
+    tool: str
+
+    def setUp(self):
+        self.tool_xml_root = etree.parse(tools_root_path / self.tool).getroot()
+        if (inputs := self.tool_xml_root.findall('./inputs')):
+            self.inputs_xml = inputs[0]
+        else:
+            self.inputs_xml = None
+        self.test_xml_list = self.tool_xml_root.findall('./tests/test')
+
+
+class MinimalTest(ToolTest):
+
+    tool = 'utils_minimal.xml'
+
+    def test_list_tests(self):
+        self.assertEqual(len(list(utils.list_tests(self.tool_xml_root))), 1)
+
+
+class IllegalTest(ToolTest):
+
+    tool = 'utils_illegal.xml'
+
+    def test_list_tests(self):
+        self.assertEqual(len(list(utils.list_tests(self.tool_xml_root))), 1)
+
+    def test_get_test_inputs1(self):
+        test_inputs = (
+            utils.get_test_inputs(self.inputs_xml, self.test_xml_list[0])
+        )
+        self.assertEqual(test_inputs, dict())
+
+
+class FullTest(ToolTest):
+
+    tool = 'utils_full.xml'
+
+    def test_list_tests(self):
+        self.assertEqual(len(list(utils.list_tests(self.tool_xml_root))), 2)
+
+    def test_get_test_inputs1(self):
+        test_inputs = (
+            utils.get_test_inputs(self.inputs_xml, self.test_xml_list[0])
+        )
+
+        # Validate input parameters without default values
+        for key in (
+            'text',
+            'integer',
+            'float',
+            'color',
+            'hidden',
+        ):
+            self.assertIsNone(test_inputs[f'{key}_without_default'], None)
+
+        # Validate input parameters with default values
+        self.assertEqual(test_inputs['text_with_default'], 'default')
+        self.assertEqual(test_inputs['integer_with_default'], 1)
+        self.assertEqual(test_inputs['float_with_default'], 1.0)
+        self.assertEqual(test_inputs['boolean_1'], 'true')
+        self.assertEqual(test_inputs['boolean_2'], 'FALSE')
+        self.assertEqual(test_inputs['color_with_default'], '#ff0000')
+        self.assertEqual(test_inputs['hidden_with_default'], 'default')
+        self.assertEqual(test_inputs['select_1'], 'default')
+
+    def test_get_test_inputs2(self):
+        test_inputs = (
+            utils.get_test_inputs(self.inputs_xml, self.test_xml_list[1])
+        )
+
+        self.assertEqual(test_inputs['text_with_default'], 'override')
+        self.assertEqual(test_inputs['text_without_default'], 'override')
+        self.assertEqual(test_inputs['integer_with_default'], 2)
+        self.assertEqual(test_inputs['integer_without_default'], 2)
+        self.assertEqual(test_inputs['float_with_default'], 2.0)
+        self.assertEqual(test_inputs['float_without_default'], 2.0)
+        self.assertEqual(test_inputs['boolean_1'], 'false')
+        self.assertEqual(test_inputs['boolean_2'], 'TRUE')
+        self.assertEqual(test_inputs['color_with_default'], '#00ff00')
+        self.assertEqual(test_inputs['color_without_default'], '#00ff00')
+        self.assertEqual(test_inputs['hidden_with_default'], 'override')
+        self.assertEqual(test_inputs['hidden_without_default'], 'override')
+        self.assertEqual(test_inputs['select_1'], 'other')
