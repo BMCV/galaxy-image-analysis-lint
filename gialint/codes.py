3c5d19027cf99a23e5dd70adc550ef26ec02ec96
Leonid Kostrykin <void@evoid.de>
2026-01-20 17:17:18 +0100

    Add `GIA102`

--- gialint/codes.py
+++ gialint/codes.py
@@ -1,3 +1,6 @@
-GIA101 = 'Use format="tabular,tsv" for table data input parameters or a superset thereof.'
+# Issues with input and output datatypes
+GIA101 = 'Use `format="tabular,tsv"` for table data input parameters or a superset thereof.'
+GIA102 = 'Use `tiff` instead of `tif`'
 
+# Issues with the `command` or `configfiles` sections
 GIA201 = 'Zarr input images require special treatment within the `command` of the tool.'
