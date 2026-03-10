a2ad4ad955a43cadfed7381c112a13371acd6d2a
Leonid Kostrykin <void@evoid.de>
2026-03-10 20:35:07 +0100

    Add option to disable checks via XML comments (#9)

--- gialint/__init__.py
+++ gialint/__init__.py
@@ -15,10 +15,20 @@ def list_codes():
             yield attr
 
 
+def _list_suppressions(code, xml_root):
+    xpath = f'.//comment()[normalize-space(.)="!{code.upper()}"]'
+    for sup in xml_root.xpath(xpath):
+        yield sup.sourceline
+
+
 def check(code, tool_xml_root, tool_path):
     check_name = f'_checks.{code.lower()}'
     check_module = __import__(check_name, globals(), locals(), fromlist=['*'], level=1)
-    yield from check_module.check(
+    suppressions = frozenset(_list_suppressions(code, tool_xml_root))
+    for info in check_module.check(
         tool_xml_root,
         tool_path.absolute() if tool_path else None,
-    )
+    ):
+        line = info if isinstance(info, int) else info['line']
+        if line not in suppressions:
+            yield info
---
+++ tests/test_check.py
@@ -0,0 +1,75 @@
+import pathlib
+import sys
+import unittest
+import unittest.mock
+
+from lxml import etree
+
+import gialint
+
+
+def _create_check_mock(lines):
+    def _check_mock(tool_xml_root, tool_path):
+        for line in lines:
+            yield line
+    return _check_mock
+
+
+class TestCase(unittest.TestCase):
+
+    code = 'gia000'
+
+    @property
+    def module_name(self):
+        return f'gialint._checks.{self.code}'
+
+    def setUp(self):
+        self.module_mock = unittest.mock.MagicMock()
+        sys.modules[self.module_name] = self.module_mock
+
+    def tearDown(self):
+        del sys.modules[self.module_name]
+
+    def test(self):
+        for suffix in (
+            '',
+            '<!-- !GIA101 -->',
+        ):
+            with self.subTest(suffix=suffix):
+                xml_test = etree.fromstring(
+                    f'''<root>
+                        <line2/>
+                        <line3/>{suffix}
+                        <line4/>
+                    </root>'''
+                )
+                self.module_mock.check = _create_check_mock([3])
+                yielded_lines = list(
+                    gialint.check(
+                        self.code,
+                        xml_test,
+                        pathlib.Path(),
+                    ),
+                )
+                self.assertEqual(
+                    yielded_lines,
+                    [3],
+                )
+
+    def test_suppressed(self):
+        xml_test = etree.fromstring(
+            f'''<root>
+                <line2/>
+                <line3/><!-- !{self.code.upper()} -->
+                <line4/>
+            </root>'''
+        )
+        self.module_mock.check = _create_check_mock([3])
+        yielded_lines = list(
+            gialint.check(
+                self.code,
+                xml_test,
+                pathlib.Path(),
+            ),
+        )
+        self.assertEqual(yielded_lines, list())
